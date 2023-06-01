ddc22c4fd7a07967a56d81adfd93e6d50dc52ce4
eslamfront55 <eslamfront55@gmail.com>
2023-06-01 02:19:19 +0200

    theme

--- src/app/core/services/themes/theme.ts
+++ src/app/core/services/themes/theme.ts
@@ -9,14 +9,20 @@ export const light: Theme = {
     "--text-main-color": " #871e35",
     "--bg-main-color": " #871e35",
     "--bg-white-color": "#fff",
+    "--bg-dark-color": "#111",
+    "--text-dark-color": "#111",
+    "--text-desc-color": "#8c95a3",
   },
 };
 
 export const dark: Theme = {
   name: "dark",
   properties: {
-    "--text-main-color": " #000",
-    "--bg-main-color": " #000",
+    "--text-main-color": " #871e35",
+    "--bg-main-color": " #871e35",
     "--bg-white-color": "#111",
+    "--bg-dark-color": "#fff",
+    "--text-dark-color": "#fff",
+    "--text-desc-color": "#a8a8a8",
   }
 }
