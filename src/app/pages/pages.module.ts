bdcf05b2432db027a71bf776ef40285f86681125
eslamfront55 <eslamfront55@gmail.com>
2023-06-01 10:23:03 +0200

    fixed

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,3 +1,4 @@
+import { CurrencyDetailsComponent } from './components/currency-details/currency-details.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './components/home/home.component';
 import { PagesRoutingModule } from './pages-routing.module';
@@ -5,9 +6,7 @@ import { SharedModule } from '../shared/shared.module';
 import { PagesComponent } from './pages.component';
 import { CoreModule } from '../core/core.module';
 import { CommonModule } from '@angular/common';
-import { AgmCoreModule } from '@agm/core';
 import { NgModule } from '@angular/core';
-import { CurrencyDetailsComponent } from './components/currency-details/currency-details.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +17,6 @@ import { CurrencyDetailsComponent } from './components/currency-details/currency
   imports: [
     ReactiveFormsModule,
     PagesRoutingModule,
-    AgmCoreModule,
     CommonModule,
     SharedModule,
     FormsModule,
